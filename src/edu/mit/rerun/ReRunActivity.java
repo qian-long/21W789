1486f3753d7aabb0dfa62a629627dd637bc69fe6
Qian Long <hungryha@gmail.com>
2012-02-27 01:10:38 -0500

    added hello world

---
+++ src/edu/mit/rerun/ReRunActivity.java
@@ -0,0 +1,36 @@
+package edu.mit.rerun;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ReRunActivity extends Activity {
+    /** Called when the activity is first created. */
+    private boolean hello = true;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        final TextView view = (TextView) findViewById(R.id.text1);
+        view.setText("hello, 21w.789");
+        Button button = (Button) findViewById(R.id.button1);
+        
+        button.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                if (hello) {
+                    view.setText("goodbye, 21w.789");
+                    hello = false;
+                }
+                else {
+                    view.setText("hello, 21w.789");
+                    hello = true;
+                }
+            }
+        });
+    }
+}
