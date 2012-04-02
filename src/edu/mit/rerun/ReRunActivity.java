0e79be8cb2b0999f005b633d02c3f52c344cff52
unknown <Hosea@.(none)>
2012-04-02 00:04:33 -0400

    commiting so I can pull

--- src/edu/mit/rerun/ReRunActivity.java
+++ src/edu/mit/rerun/ReRunActivity.java
@@ -1,6 +1,8 @@
 package edu.mit.rerun;
 
+import edu.mit.rerun.view.*;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,24 +14,37 @@ public class ReRunActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        final TextView view = (TextView) findViewById(R.id.text1);
-        view.setText("hello, 21w.789");
-        Button button = (Button) findViewById(R.id.button1);
+        setContentView(R.layout.log_in);
         
-        button.setOnClickListener(new View.OnClickListener() {
-            
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                if (hello) {
-                    view.setText("goodbye, 21w.789");
-                    hello = false;
-                }
-                else {
-                    view.setText("hello, 21w.789");
-                    hello = true;
-                }
+        Button next = (Button) findViewById(R.id.logInButton);
+        next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+            	
+            	// TODO- temporary code here for view change
+            	
+            	setContentView(R.layout.postitem);
             }
+
         });
+        
+        
+//        final TextView view = (TextView) findViewById(R.id.textView1);
+//        view.setText("hello, 21w.789");
+//        Button button = (Button) findViewById(R.id.button1);
+//        
+//        button.setOnClickListener(new View.OnClickListener() {
+//            
+//            public void onClick(View v) {
+//                // TODO Auto-generated method stub
+//                if (hello) {
+//                    view.setText("goodbye, 21w.789");
+//                    hello = false;
+//                }
+//                else {
+//                    view.setText("hello, 21w.789");
+//                    hello = true;
+//                }
+//            }
+//        });
     }
 }
