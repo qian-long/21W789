b59c0437f5e460117ecdd02133c0c851da3af8bb
unknown <Hosea@.(none)>
2012-04-16 03:52:02 -0400

    added filter icon and functionality

--- src/edu/mit/rerun/view/ItemListActivity.java
+++ src/edu/mit/rerun/view/ItemListActivity.java
@@ -23,41 +23,50 @@ import android.widget.ImageButton;
  */
 public class ItemListActivity extends ListActivity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_item_list);
-        ImageButton postButton = (ImageButton) findViewById(R.id.postButton);
-        ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
-        
-        postButton.setOnClickListener(new View.OnClickListener() {
-            
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(v.getContext(), PostItemActivity.class);
-                startActivity(intent);
-            }
-        });
-        
-        addButton.setOnClickListener(new View.OnClickListener() {
-            
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent intent = new Intent(v.getContext(), EditFilterActivity.class);
-                startActivity(intent);
-            }
-        });
-        List<ReuseItem> samples = new ArrayList<ReuseItem>();
-        samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
-        samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
-        samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
-        samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
-        samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
-        samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_item_list);
+		ImageButton filterButton = (ImageButton) findViewById(R.id.filterButton);
+		ImageButton postButton = (ImageButton) findViewById(R.id.postButton);
+		ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
 
-        ItemListAdapter adapter = new ItemListAdapter(this, (ArrayList<ReuseItem>)samples);
-        setListAdapter(adapter);
+		filterButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(v.getContext(), FilterSettingsActivity.class);
+				startActivity(intent);
+			}
+		});
 
-    }
+		postButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(v.getContext(), PostItemActivity.class);
+				startActivity(intent);
+			}
+		});
+
+		addButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(v.getContext(), EditFilterActivity.class);
+				startActivity(intent);
+			}
+		});
+		List<ReuseItem> samples = new ArrayList<ReuseItem>();
+		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
+
+		ItemListAdapter adapter = new ItemListAdapter(this, (ArrayList<ReuseItem>)samples);
+		setListAdapter(adapter);
+
+	}
 
 }
