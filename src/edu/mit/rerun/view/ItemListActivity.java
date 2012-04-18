593cdbea4a08a09c91cf058434c5720fcb8512d4
unknown <Hosea@.(none)>
2012-04-17 20:35:25 -0400

    added arrows for filters

--- src/edu/mit/rerun/view/ItemListActivity.java
+++ src/edu/mit/rerun/view/ItemListActivity.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import edu.mit.rerun.R;
 import edu.mit.rerun.model.ReuseItem;
+import edu.mit.rerun.utils.DatabaseAdapter;
 import edu.mit.rerun.utils.ItemListAdapter;
 import android.app.Activity;
 import android.app.ListActivity;
@@ -14,6 +15,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -25,6 +28,9 @@ import android.widget.ImageButton;
 public class ItemListActivity extends ListActivity {
 	public static final int ADD_FILTER_RESULT = 0;
 	private Context mContext = this;
+	public DatabaseAdapter dba;
+	private int currentFilterIndex = 0;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -33,6 +39,14 @@ public class ItemListActivity extends ListActivity {
 		ImageButton postButton = (ImageButton) findViewById(R.id.postButton);
 		ImageButton addButton = (ImageButton) findViewById(R.id.addButton);
 
+		ImageButton forwardFilterButton = (ImageButton) findViewById(R.id.forwardFilterBtn);
+		ImageButton backFilterButton = (ImageButton) findViewById(R.id.backFilterBtn);
+		ImageButton refreshButton = (ImageButton) findViewById(R.id.refreshBtn);
+
+		final TextView filterName = (TextView) findViewById(R.id.filterName);
+		
+		dba = new DatabaseAdapter(mContext);
+		
 		filterButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 				Intent intent = new Intent(v.getContext(), FilterSettingsActivity.class);
@@ -41,7 +55,6 @@ public class ItemListActivity extends ListActivity {
 		});
 
 		postButton.setOnClickListener(new View.OnClickListener() {
-
 			public void onClick(View v) {
 				Intent intent = new Intent(v.getContext(), PostItemActivity.class);
 				startActivity(intent);
@@ -49,13 +62,39 @@ public class ItemListActivity extends ListActivity {
 		});
 
 		addButton.setOnClickListener(new View.OnClickListener() {
-
 			public void onClick(View v) {
 				Intent intent = new Intent(v.getContext(), EditFilterActivity.class);
 				//				startActivity(intent);
 				startActivityForResult(intent, ADD_FILTER_RESULT);
 			}
 		});
+		
+		forwardFilterButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				dba.open();
+				currentFilterIndex++;
+				if (currentFilterIndex == dba.getAllFilters().size()) {
+					currentFilterIndex = 0;
+				}
+				String displayName = dba.getAllFilters().get(currentFilterIndex).getFiltername();
+				filterName.setText(displayName);
+				
+				dba.close();
+			}
+		});
+		
+		backFilterButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				dba.open();
+				currentFilterIndex--;
+				if (currentFilterIndex < 0 ) {
+					currentFilterIndex = dba.getAllFilters().size()-1;
+				}
+				String displayName = dba.getAllFilters().get(currentFilterIndex).getFiltername();
+				filterName.setText(displayName);
+				dba.close();			}
+		});
+		
 		List<ReuseItem> samples = new ArrayList<ReuseItem>();
 		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
 		samples.add(new ReuseItem("id", "sender", "title", "description", "location", "4/16/2012", 123456, 234456));
@@ -78,6 +117,6 @@ public class ItemListActivity extends ListActivity {
 
 		}
 	}
-
-
+	
+	
 }
